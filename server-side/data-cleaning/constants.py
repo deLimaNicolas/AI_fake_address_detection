eda39f63f8a6e4f70d590bae5a3e788d9f78bd7c
Nicolas Lima <38044236+deLimaNicolas@users.noreply.github.com>
2020-06-30 13:23:07 -0300

    Update constants.py

--- server-side/data-cleaning/constants.py
+++ server-side/data-cleaning/constants.py
@@ -1,3 +1,3 @@
-GOOGLE_KEY = "AIzaSyCUL8uNR84N8dKpI8o487yyZLwaOOms15I"
+GOOGLE_KEY = ""
 MAP_BOX_URL = "https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/"
-MAP_BOX_KEY = "pk.eyJ1IjoidGVpeGxpbWEiLCJhIjoiY2sxd3h4Nm4xMDZxazNjbXRydGhmNnFlMyJ9.YEv9Vtj0wApHDAgFVtrafg"
+MAP_BOX_KEY = ""
